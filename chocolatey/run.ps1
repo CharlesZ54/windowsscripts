bda60cd0addcd489115ca434dec857fb4af60b20
Charles Zwicker <charles@zwicker.me>
2020-10-09 10:13:26 -0400

    Update run.ps1

--- chocolatey/run.ps1
+++ chocolatey/run.ps1
@@ -1,8 +1,9 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco feature enable -n allowGlobalConfirmation
 $programs = Get-Content $PSScriptRoot\programs.txt
 $prerelease = Get-Content $PSScriptRoot\prerelease.txt
-choco install -y $programs
-choco install -y -pre $prerelease
+choco install $programs
+choco install -pre $prerelease
 Set-ExecutionPolicy Default -Scope Process -Force
